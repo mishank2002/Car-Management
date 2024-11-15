899bef2f9ac30830da80156c9f69ab9afe49e3e2
Mishank Kumar <96677559+mishank2002@users.noreply.github.com>
2024-11-15 13:07:51 +0530

    Update UserRoutes.js

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/UserController');
 const router = express.Router();
 
 // Route for registering a new user
